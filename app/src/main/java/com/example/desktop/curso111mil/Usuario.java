43e37b91af5a588d244597acdf9a0bb05c2aedbc
juancho <jtimetraveler@gmail.com>
2017-08-11 17:14:59 -0300

    Prueba del jueves

---
+++ app/src/main/java/com/example/desktop/curso111mil/Usuario.java
@@ -0,0 +1,34 @@
+package com.example.desktop.curso111mil;
+
+/**
+ * Created by juancho on 11/08/17.
+ */
+
+public class Usuario {
+
+    private String nombre, contrasenia, mail;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
